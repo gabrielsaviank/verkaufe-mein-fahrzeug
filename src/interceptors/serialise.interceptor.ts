0a9dc1d23a215535dcacec0f2050d181bde719c8
gabrielsavian <gabrielsavian@gmail.com>
2021-09-14 20:31:37 -0300

    chore: some types

--- src/interceptors/serialise.interceptor.ts
+++ src/interceptors/serialise.interceptor.ts
@@ -8,19 +8,26 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
 
-import { UserDto } from '../users/dtos/user.dto';
+interface ClassConstructor {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  new (...args: any[]): {};
+}
 
+export function Serialise(dto: ClassConstructor) {
+  return UseInterceptors(new SerialiseInterceptor(dto));
+}
 export class SerialiseInterceptor implements NestInterceptor {
   // Intercepts != extends
+  constructor(private dto: any) {}
   intercept(
     context: ExecutionContext,
-    next: CallHandler<any>,
+    next: CallHandler,
   ): Observable<any> | Promise<Observable<any>> {
     // ACHTUNG! This runs something before a reques is handled by the request handler
     return next.handle().pipe(
       map((data: any) => {
         // run something before the response is sent out
-        return plainToClass(UserDto, data, {
+        return plainToClass(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
