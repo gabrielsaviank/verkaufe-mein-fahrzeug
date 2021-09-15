b1c4b318dde813e0771264937a6950fd54ae8b29
gabrielsavian <gabrielsavian@gmail.com>
2021-09-15 18:39:13 -0300

    feat: hashing users that sign up

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
 import { UsersService } from './users.service';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -9,14 +13,20 @@ export class AuthService {
   async signup(email: string, password: string) {
     //check if email is in use
     const users = await this.usersService.find(email);
+
     if (users.length) {
       throw new BadRequestException('Email is already in use');
     }
     // Hash users password
+    const salt = randomBytes(8).toString('hex');
 
-    // Create a new user and save it
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
 
-    // return the user
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.usersService.create(email, result);
+
+    return user;
   }
 
   signin(email: string, password: string) {}
