297404878600a21dcd952b2430356f64688c21b2
gabrielsavian <gabrielsavian@gmail.com>
2021-09-24 08:24:52 -0300

    feat:interceptors and decorators to map userIds

---
+++ src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,29 @@
+/*This will allow us to get access to the users service and provide to our decorator
+that will provide our userId o our controller. (A bit messy but okay)
+*/
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Injectable,
+} from '@nestjs/common';
+
+import { UsersService } from '../users.service';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private usersService: UsersService) {}
+
+  async intercept(context: ExecutionContext, handler: CallHandler) {
+    // Find our user
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session || {};
+
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
+      request.currentUser = user;
+    }
+
+    return handler.handle();
+  }
+}
